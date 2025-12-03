18675b0b0c9a959d160f1d384ae00673b21c8833
SagarAigal <sagargajanan.a@arche.global>
2025-12-03 22:25:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello New World!'));
 app.listen(3000, () => console.log('App running on port 3000...'));
+
